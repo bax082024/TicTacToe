e212ee8eebbd6032d9866b3d8e832457c2d6f4f5
bax082024 <bax082024@gmail.com>
2024-12-25 23:51:45 +0100

    added btns + panel + label

--- TicTacToeForm.cs
+++ TicTacToeForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TicTacToe
 {
-    public partial class Form1 : Form
+    public partial class TicTacToeForm : Form
     {
-        public Form1()
+        public TicTacToeForm()
         {
             InitializeComponent();
         }
